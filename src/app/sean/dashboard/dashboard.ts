b26d53415e37faa9bf22b0ca233e02843fea6ad9
MichaelOlave <michaelolave12@gmail.com>
2025-11-03 14:43:44 -0500

    add initial Sean's dashboard component with HTML, CSS, and test files

---
+++ src/app/sean/dashboard/dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sean-ashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {}
