179414a3364efaf18013b086740a978e10255be3
MichaelOlave <michaelolave12@gmail.com>
2025-11-03 14:42:54 -0500

    add initial Yeskey's dashboard component with HTML and CSS files

---
+++ src/app/yeskey/dashboard/dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yeskey-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {}
