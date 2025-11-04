9221d754e035490d02a2731d75e6c242734236f7
MichaelOlave <michaelolave12@gmail.com>
2025-11-04 16:20:49 -0500

    Refactor Bingo component to streamline board management and update HTML structure

--- src/app/michael/bingo/bingo.ts
+++ src/app/michael/bingo/bingo.ts
@@ -1,111 +1,18 @@
-import { AfterViewInit, Component, input } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Board } from '../components/board/board';
 
 @Component({
   selector: 'app-bingo',
   templateUrl: './bingo.html',
   styleUrls: ['./bingo.css'],
+  imports: [Board],
 })
-export class Bingo implements AfterViewInit {
-  fontSize = input(24);
-  tileSize = input(100);
-  boardSize = input(5);
-  boardConfig: { column: string; range: [number, number]; freeSpace: boolean }[] = [
-    { column: 'B', range: [1, 15], freeSpace: false },
-    { column: 'I', range: [16, 30], freeSpace: false },
-    { column: 'N', range: [31, 45], freeSpace: true },
-    { column: 'G', range: [46, 60], freeSpace: false },
-    { column: 'O', range: [61, 75], freeSpace: false },
-  ];
-  addColumn() {}
-  removeColumn() {}
-  generateValues(range: [number, number] = [1, 15]): number[] {
-    const tileNumbers: number[] = [];
-    const size = this.boardSize();
-    for (let i = 0; i < size; i++) {
-      const ranNum = Math.floor(Math.random() * (range[1] - range[0] + 1) + range[0]);
-      if (tileNumbers.includes(ranNum)) {
-        i--;
-        continue;
-      }
-      tileNumbers.push(ranNum);
-    }
-    return tileNumbers;
-  }
-  tile(value: number): HTMLElement {
-    const gridTile = document.createElement('div');
-    gridTile.style.width = `${this.tileSize()}px`;
-    gridTile.style.height = `${this.tileSize()}px`;
-    gridTile.style.border = 'solid';
-    gridTile.style.backgroundColor = 'gray';
-    gridTile.onmouseenter = () => {
-      if (gridTile.style.backgroundColor !== 'red') {
-        gridTile.style.backgroundColor = 'lightgray';
-      }
-    };
-    gridTile.onmouseleave = () => {
-      if (gridTile.style.backgroundColor !== 'red') {
-        gridTile.style.backgroundColor = 'gray';
-      }
-    };
-    gridTile.style.cursor = 'pointer';
-    gridTile.style.fontSize = `${this.fontSize()}px`;
-    gridTile.style.alignContent = 'center';
-    gridTile.style.textAlign = 'center';
-    gridTile.textContent = String(value);
-    if (value === 0) {
-      gridTile.textContent = 'FREE';
-    }
-    gridTile.onclick = () => {
-      gridTile.style.backgroundColor = gridTile.style.backgroundColor === 'red' ? 'gray' : 'red';
-    };
-    return gridTile;
-  }
-  headerTile(value: string): HTMLElement {
-    const headerTile = document.createElement('div');
-    headerTile.style.width = `${this.tileSize()}px`;
-    headerTile.style.height = `${this.tileSize()}px`;
-    headerTile.style.backgroundColor = 'white';
-    headerTile.style.fontSize = `${this.fontSize() + 25}px`;
-    headerTile.style.alignContent = 'center';
-    headerTile.style.textAlign = 'center';
-    headerTile.textContent = value;
-    return headerTile;
-  }
-  tileColumn(boardConfig: {
-    column: string;
-    range: [number, number];
-    freeSpace: boolean;
-  }): HTMLElement {
-    const col = document.createElement('div');
-    col.style.display = 'flex';
-    col.style.flexDirection = 'column';
+export class Bingo {
+  @ViewChild(Board) boardComp?: Board;
+  @ViewChild(Board) cpuBoard?: Board;
 
-    col.appendChild(this.headerTile(boardConfig.column));
-
-    const values = this.generateValues(boardConfig.range);
-
-    const size = this.boardSize();
-
-    if (boardConfig.freeSpace) {
-      values[Math.ceil(size / 2 - 1)] = 0;
-    }
-
-    for (let i = 0; i < size; i++) {
-      col.appendChild(this.tile(values[i]));
-    }
-    return col;
+  addBoard(): void {
+    this.boardComp?.loadBoard('gameBoard');
   }
-  loadBoard(): void {
-    const gameBoard = document.getElementById('gameBoard');
-    if (!gameBoard) return;
-    gameBoard.style.display = 'flex';
-    gameBoard.style.justifyContent = 'center';
-
-    this.boardConfig.forEach((col) => {
-      gameBoard.appendChild(this.tileColumn(col));
-    });
-  }
-  ngAfterViewInit(): void {
-    this.loadBoard();
-  }
+  addCPUBoard(): void {}
 }
