a6f5718b1b941b369270a2abfd536e9d0a8f4ac7
MichaelOlave <michaelolave12@gmail.com>
2025-11-04 12:38:04 -0500

    Refactor Header component and update styles for improved layout and navigation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  title = signal('Angular Final');
+  title = signal('Project Gallary');
+  pageTitle = signal('');
 }
