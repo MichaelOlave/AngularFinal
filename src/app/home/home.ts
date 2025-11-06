165c384c69f414e4a3b1efde327cae0cf7066bef
MichaelOlave <michaelolave12@gmail.com>
2025-11-06 17:01:35 -0500

    feat: implement hero section with NgxDarkVeil for enhanced UI

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NgxDarkVeilComponent } from '@omnedia/ngx-dark-veil';
+import { NgxGradientTextComponent } from '@omnedia/ngx-gradient-text';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgxDarkVeilComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
