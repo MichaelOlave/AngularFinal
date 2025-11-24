9c98858ec8e7c2717ae49f25e67a87bee8a0a439
Michael Olave <michaelolave12@gmail.com>
2025-11-24 10:30:19 -0500

    add film-card component with template, styles, and tests

---
+++ src/app/michael/components/film-card/film-card.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+export type Film = {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+};
+
+@Component({
+  selector: 'app-film-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './film-card.html',
+  styleUrl: './film-card.css',
+})
+export class FilmCard {
+  @Input() film!: Film;
+  constructor() {
+    console.log(this.film);
+  }
+}
