b6ff6cabd64a8f794df4b09b1e527ac139d863b8
seanhammond17 <sean.hammond@mymail.champlain.edu>
2025-12-01 15:37:50 -0500

    Title changes

--- src/app/components/projectgallery/projectgallery.ts
+++ src/app/components/projectgallery/projectgallery.ts
@@ -33,7 +33,7 @@ export class Projectgallery {
       author: 'Michael Olave',
     },
     {
-      title: 'Mineclicker',
+      title: 'Mineclick',
       component: Mineclick,
       route: '/sean/dashboard',
       author: 'Sean',
